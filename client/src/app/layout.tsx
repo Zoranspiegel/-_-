323afd6c5fdca0659ca0011c34d8834cf6670bfd
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-19 18:35:46 -0500

    C:/Program Files/Git/client landing page

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Courier_Prime } from 'next/font/google';
+import './globals.css';
+
+const font = Courier_Prime({ weight: '400', subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Мрачне тајне',
+  description: 'Secrets from the world`s swamp'
+};
+
+export default function RootLayout ({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>): JSX.Element {
+  return (
+    <html lang="en">
+      <body className={`${font.className} text-[green] bg-cover bg-center select-none`} style={{ backgroundImage: 'url(https://res.cloudinary.com/dkc8xrlg8/image/upload/v1708254931/ASCII%20Images/Sewers.png)' }}>{children}</body>
+    </html>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Home (): JSX.Element {
+  return (
+    <main className='min-h-screen flex flex-col justify-center items-center'>
+      <div className=' flex flex-col w-full max-w-xs rounded-lg border-4 border-double border-[green] bg-[rgba(0,0,0,0.7)] p-4 gap-4'>
+        <h1 className=' text-center text-xl font-bold'>Мрачне тајне</h1>
+        <Link
+          href='/login'
+          className='rounded-sm border-4 border-double border-[green] text-center text-[green] text-lg font-bold py-2 hover:border-black hover:bg-[green] hover:text-black'
+        >Sign up</Link>
+        <Link
+          href='/signup'
+          className='rounded-sm border-4 border-double border-[green] text-center text-[green] text-lg font-bold py-2 hover:border-black hover:bg-[green] hover:text-black'
+        >Sign in</Link>
+      </div>
+    </main>
+  );
+}
