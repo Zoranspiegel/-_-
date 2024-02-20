54037cf2f22502b697fe66cd9f60bbfe77015e98
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-20 04:24:11 -0500

    production db

--- client/src/db.ts
+++ client/src/db.ts
@@ -5,15 +5,21 @@ const appDir = process.cwd();
 loadEnvConfig(appDir);
 
 export function getClient (): Client {
-  const client = new Client({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    port: Number(process.env.PG_PORT)
-  });
-
-  return client;
+  if (process.env.POSTGRES_URL !== null && process.env.POSTGRES_URL !== undefined) {
+    const client = new Client({
+      connectionString: process.env.POSTGRES_URL + '?sslmode=require'
+    });
+    return client;
+  } else {
+    const client = new Client({
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DB,
+      port: Number(process.env.PG_PORT)
+    });
+    return client;
+  }
 }
 
 getClient();
