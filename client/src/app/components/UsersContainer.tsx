a7ae9945196a29519c7e38286a2f73c9cb64f68f
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-22 16:55:31 -0500

    UsersContainer component

---
+++ client/src/app/components/UsersContainer.tsx
@@ -0,0 +1,30 @@
+import useSWR from 'swr';
+import User from './User';
+
+export default function UsersContainer ({
+  url,
+  id,
+  page
+}: {
+  url: string
+  id?: string
+  page: number
+}): JSX.Element {
+  const { data, isLoading, error } = useSWR(`${url}${id ? `/${id}/` : ''}?page=${page}`);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+
+  const user: UserProfile[] = data;
+  return (
+    <div>
+      {user?.map(user => (
+        <User
+          key={user.id}
+          user={user}
+          personal={user.id === id}
+        />
+      ))}
+    </div>
+  );
+}
