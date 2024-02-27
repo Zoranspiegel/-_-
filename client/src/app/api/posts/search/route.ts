97794a98ba0ffdc9afbc4f14944b4c114d4756e2
Juan José Rodriguez <zoranbow@gmail.com>
2024-02-27 17:50:35 -0500

    next/api/posts/search by content endpoint

---
+++ client/src/app/api/posts/search/route.ts
@@ -0,0 +1,28 @@
+import { getClient } from '@/db';
+import { NextResponse } from 'next/server';
+
+export async function GET (request: Request): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
+  const content = searchParams.get('content');
+  const page = Number(searchParams.get('page')) || 0;
+  const limit = 5;
+  const offset = limit * page;
+
+  const client = getClient();
+  await client.connect();
+
+  const foundPostsRes = await client.query(
+    `select p.*, u.username, u.avatar, u.is_admin from posts p 
+    inner join users u on p.user_id = u.id 
+    where content ilike $1 
+    order by created_at desc limit $2 offset $3`,
+    ['%' + content + '%', limit + 1, offset]
+  );
+
+  await client.end();
+
+  const posts = foundPostsRes.rows.slice(0, limit);
+  const last = foundPostsRes.rows.length < limit + 1;
+
+  return NextResponse.json({ posts, last }, { status: 200 });
+}
